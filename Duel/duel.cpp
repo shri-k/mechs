5e4d4fa1eed5ca4b2197221eb8faa30e81ac634f
shri-k <shrirampmp@gmail.com>
2023-08-13 13:39:07 -0700

    updated duel

--- Duel/duel.cpp
+++ Duel/duel.cpp
@@ -1,17 +1,24 @@
+//iostream = print out/scanner
+//string self explanatory
+//windows and uni are for the sleep() fnction
+//playercard.h player and card classes defined in that file  
 #include <iostream>
 #include <string>
 #include <windows.h>
-#include <unistd.h>  
+#include <unistd.h>
 #include "playercard.h"
 
+//so i dont have to write std::cout and can just write cout etc
 using namespace std;
 
 int main() {
-
+    //seeds the random number generator
     srand(time(0));
 
+    //declare 2 players and put them in an array to access easily
     player player0;
-    player player1; 
+    player player1;
+    player players[2] = {player0, player1}; 
 
     // alternate bnetween 0 and 1 to keep track of whose turn it is.
     bool turn = 0;
@@ -20,9 +27,10 @@ int main() {
 
         cout << "player " << turn << "'s turn: " << endl;
         cout << "type any name to draw a card which will be named (1 word max)" << endl;
+
+        //user input
         string s;
         cin >> s;
-
         if (s == "quit") {
             break;
         }
@@ -31,18 +39,20 @@ int main() {
         sleep(1);
         cout << "player " << turn << " draws " << s << endl;
 
+        //generate random values
         int atk = rand()%2000;
         int def = rand()%2000;
         cout << "atk: " << atk << " def: " << def << endl;
 
-        if (turn == 0) {player0.drawCard(s, atk, def);}
-        else if (turn == 1) {player1.drawCard(s, atk, def);}
+        //draw card for the player using those values
+        players[turn].drawCard(s, atk, def);
 
         sleep(1);
         cout << "------------hand------------ " << endl;
 
-        if (turn == 0) {player0.printHand();}
-        else if (turn == 1) {player1.printHand();}
+        //print hand for player
+        players[turn].printHand();
+        
         
 
 
@@ -53,7 +63,7 @@ int main() {
 
 
         
-
+        //alternate the turn
         if (turn == 0) {turn = 1;}
         else if (turn == 1) {turn = 0;}
 
@@ -64,6 +74,6 @@ int main() {
 
     }
 
-
+    
     return 0;
 }
